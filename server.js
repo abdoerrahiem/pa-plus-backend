6d165c1017646681d18c372f81168cb6ce188e44
Abdur Rahim <abdoerrahiem@gmail.com>
2021-05-17 09:24:14 +0700

    edited routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
+const paket = require('./data/paket.json')
+const calculate = require('./data/calculate.json')
 
 const app = express()
 
@@ -13,10 +15,28 @@ app.get('/', (req, res) => {
   res.send('Server running...')
 })
 
-fs.readdirSync('./routes').map((route) => {
-  route = route.split('.')[0]
+// fs.readdirSync('./routes').map((route) => {
+//   route = route.split('.')[0]
 
-  return app.use(`/api/v1/${route}`, require(`./routes/${route}`))
+//   return app.use(`/api/v1/${route}`, require(`./routes/${route}`))
+// })
+
+app.get('/paplus/api/plan', (req, res) => {
+  res.json({
+    success: true,
+    data: paket,
+  })
+})
+
+app.post('/paplus/api/quotes/calculate_premium', (req, res) => {
+  const { plan } = req.body
+
+  const foundedCalculate = calculate.find((pk) => pk.id === plan)
+
+  res.json({
+    success: true,
+    data: foundedCalculate,
+  })
 })
 
 const PORT = process.env.PORT || 5000
