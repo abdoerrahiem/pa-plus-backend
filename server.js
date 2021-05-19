ad7413523db1a09bc6e5ae54712133ca87778e65
Abdur Rahim <abdoerrahiem@gmail.com>
2021-05-19 15:49:05 +0700

    added create quote route

--- server.js
+++ server.js
@@ -22,21 +22,196 @@ app.get('/', (req, res) => {
 // })
 
 app.get('/paplus/api/plan', (req, res) => {
-  res.json({
-    success: true,
-    data: paket,
-  })
+  res.json(paket)
 })
 
 app.post('/paplus/api/quotes/calculate_premium', (req, res) => {
   const { plan } = req.body
 
-  const foundedCalculate = calculate.find((pk) => pk.id === plan)
+  const response = {
+    total_premium:
+      plan === 'PAPF1'
+        ? 1100000
+        : plan === 'PAPF2'
+        ? 1300000
+        : plan === 'PAPF3'
+        ? 1500000
+        : plan === 'PAPI1'
+        ? 300000
+        : plan === 'PAPI2'
+        ? 350000
+        : 0,
+    total_discount: 0,
+    total_admin_fee: 0,
+    total_stamp_duty: 0,
+    total_payable:
+      plan === 'PAPF1'
+        ? 1100000
+        : plan === 'PAPF2'
+        ? 1300000
+        : plan === 'PAPF3'
+        ? 1500000
+        : plan === 'PAPI1'
+        ? 300000
+        : plan === 'PAPI2'
+        ? 350000
+        : 0,
+    total_donation: 0,
+    discount_pct: 0,
+    premium_details: [
+      {
+        amount:
+          plan === 'PAPF1'
+            ? 1100000
+            : plan === 'PAPF2'
+            ? 1300000
+            : plan === 'PAPF3'
+            ? 1500000
+            : plan === 'PAPI1'
+            ? 300000
+            : plan === 'PAPI2'
+            ? 350000
+            : 0,
+        amount_detail:
+          plan === 'PAPF1'
+            ? 1100000
+            : plan === 'PAPF2'
+            ? 1300000
+            : plan === 'PAPF3'
+            ? 1500000
+            : plan === 'PAPI1'
+            ? 300000
+            : plan === 'PAPI2'
+            ? 350000
+            : 0,
+        coverage_code: 'PA-P',
+        coverage_detail: 'PA Plus',
+        admin_fee: 0,
+        stamp_duty: 0,
+        rate:
+          plan === 'PAPF1'
+            ? 1100000
+            : plan === 'PAPF2'
+            ? 1300000
+            : plan === 'PAPF3'
+            ? 1500000
+            : plan === 'PAPI1'
+            ? 300000
+            : plan === 'PAPI2'
+            ? 350000
+            : 0,
+      },
+    ],
+  }
 
-  res.json({
-    success: true,
-    data: foundedCalculate,
-  })
+  res.json(response)
+})
+
+app.post('/paplus/api/quotes', (req, res) => {
+  const { inception_date, plan } = req.body
+
+  const response = {
+    product_id: 'PA-P',
+    sum_insured_1: 1,
+    sum_insured_2: 0,
+    sum_insured_3: 0,
+    sum_insured_4: 0,
+    sum_insured_5: 0,
+    include_ext_cover_f: 0,
+    inception_date,
+    coverage_id: 'ECHAN-PAP',
+    id: 21359,
+    total_premium:
+      plan === 'PAPF1'
+        ? 1100000
+        : plan === 'PAPF2'
+        ? 1300000
+        : plan === 'PAPF3'
+        ? 1500000
+        : plan === 'PAPI1'
+        ? 300000
+        : plan === 'PAPI2'
+        ? 350000
+        : 0,
+    total_payable:
+      plan === 'PAPF1'
+        ? 1100000
+        : plan === 'PAPF2'
+        ? 1300000
+        : plan === 'PAPF3'
+        ? 1500000
+        : plan === 'PAPI1'
+        ? 300000
+        : plan === 'PAPI2'
+        ? 350000
+        : 0,
+    total_discount: 0,
+    total_admin_fee: 0,
+    total_stamp_duty: 0,
+    donation: 0,
+    dicount_pct: 0,
+    expire_date: inception_date,
+    charity_organization: null,
+    paplus_detail: {
+      occupation_id: '',
+      sum_insured_level: '',
+      gender: '',
+      birth_date: inception_date,
+      name: '',
+      id_number: '',
+      b_flag: false,
+      c_flag: false,
+      beneficiary_name: '',
+      beneficiary_contact: '',
+    },
+    customer_detail: null,
+    premium_details: [
+      {
+        amount:
+          plan === 'PAPF1'
+            ? 1100000
+            : plan === 'PAPF2'
+            ? 1300000
+            : plan === 'PAPF3'
+            ? 1500000
+            : plan === 'PAPI1'
+            ? 300000
+            : plan === 'PAPI2'
+            ? 350000
+            : 0,
+        amount_detail:
+          plan === 'PAPF1'
+            ? 1100000
+            : plan === 'PAPF2'
+            ? 1300000
+            : plan === 'PAPF3'
+            ? 1500000
+            : plan === 'PAPI1'
+            ? 300000
+            : plan === 'PAPI2'
+            ? 350000
+            : 0,
+        coverage_code: 'PA-P',
+        coverage_detail: 'PA Plus',
+        admin_fee: 0,
+        stamp_duty: 0,
+        rate:
+          plan === 'PAPF1'
+            ? 1100000
+            : plan === 'PAPF2'
+            ? 1300000
+            : plan === 'PAPF3'
+            ? 1500000
+            : plan === 'PAPI1'
+            ? 300000
+            : plan === 'PAPI2'
+            ? 350000
+            : 0,
+      },
+    ],
+  }
+
+  res.json(response)
 })
 
 const PORT = process.env.PORT || 5000
